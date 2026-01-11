16228f192b78be29c5a99ffda11ea652be105c1e
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2026-01-11 18:52:08 -0500

    exercise 5 complete

--- js/app.js
+++ js/app.js
@@ -68,8 +68,18 @@ function updateMessage() {
     };
 };
 
-
 //5) Define the required constants.
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 //6) Handle a player clicking a square with a `handleClick` function.
 
